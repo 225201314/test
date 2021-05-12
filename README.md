918d4ba26f9c69074f0e6918fc4ab26fb98635af
225201314 <84030542+225201314@users.noreply.github.com>
2021-05-12 08:38:35 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Youtube Downloader
+
+This is a simple http server for recive youtube video links and download.
+
+# TODO
+
+- [ ] add home page with a download form
+- [ ] ffmpeg auto update
+- [ ] get stdout and pretty print.
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	ytdl "github.com/kkdai/youtube/v2/downloader"
+)
+
+func main() {
+	http.HandleFunc("/", ytdlHandler)
+	log.Println(http.ListenAndServe(":8080", nil))
+	log.Println("Youtube Downloader running on :8080")
+}
+
+func ytdlHandler(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query().Get("v")
+	q := r.URL.Query().Get("q")
+	if v == "" {
+		http.Error(w, "Hello world!\n", http.StatusBadRequest)
+	} else {
+		if err := Download(v, q); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+// Download will download youtube video by src and qulity,
+// src is the video url or video id,
+// quality can be hd720 or hd1080 etc., default is medium
+func Download(src string, quality string) error {
+	dl := ytdl.Downloader{}
+	// dl.Debug = true
+	v, err := dl.Client.GetVideo(src)
+	if err != nil {
+		return err
+	}
+
+	if quality == "" {
+		quality = "hd720"
+	}
+	vfmt := v.Formats.FindByQuality(quality)
+
+	root, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	dl.OutputDir = filepath.Join(root, "Downloads")
+
+	if strings.HasPrefix(quality, "hd") {
+		if err = checkFFMPEG(); err != nil {
+			return err
+		}
+		return dl.DownloadComposite(context.Background(), "", v, quality, "mp4")
+	}
+
+	return dl.Download(context.Background(), v, vfmt, "")
+}
+
+func checkFFMPEG() error {
+	fmt.Println("check ffmpeg is installed....")
+	if err := exec.Command("ffmpeg", "-version").Run(); err != nil {
+		return fmt.Errorf("please check ffmpegCheck is installed correctly")
+	}
+
+	return nil
+}
